320b825093d323439b5d5ba1ebe40ef57ce376e4
sneppy <andrea.mecchia@mail.polimi.it>
2020-05-24 19:52:19 +0200

    Closed #7 + Card constructor accepts strings and CardRank.ACE

--- pokerl/cards.py
+++ pokerl/cards.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List, Tuple
 from pokerl.enums import CardRank, CardSuit
 
 class Card:
@@ -6,20 +6,48 @@ class Card:
 
 	@property
 	def rank(self) -> int:
-		"""  """
+		"""Returns the rank of the card
+		
+		The rank of an ace is 13 and not 0"""
 
 		return (self.value & 0xf) or 13 # Make ace highest rank
 	
 	@property
 	def suit(self) -> int:
-		"""  """
+		""" Returns the suit of the card """
 
 		return self.value >> 4
 	
-	def __init__(self, value: Union[int, tuple]):
-		"""  """
+	def __init__(self, value: Union[int, str, Tuple[int]]):
+		"""Construct a new card
 		
-		if isinstance(value, tuple): value = (value[1] << 4) | value[0]
+		Params
+		------
+		value : int, str or tuple of ints
+			If `value` has type `int`, it is
+			interpreted as `(suit << 4) |
+			rank`;
+			if `value` is a string, the first
+			character is a rank symbol (e.g.
+			'A', '2', '6', 'K', 'T', etc.)
+			and the second character is a suit
+			symbol (i.e. 'S', 'H', 'D' and 'C');
+			If `value` is a tuple, the first
+			value is the rank and the second
+			value is the suit.
+		"""
+		
+		if isinstance(value, tuple):
+			# From (suit, rank) tuple
+			rank, suit = value
+			if rank == CardRank.ACE: rank = CardRank.ONE
+			value = (suit << 4) | rank
+		elif isinstance(value, str):
+			# From 'RS' string
+			rank, suit = CardRank.from_symbol[value[0]], CardSuit.from_symbol[value[1]]
+			if rank == CardRank.ACE: rank = CardRank.ONE
+			value = (suit << 4) | rank
+
 		assert (value & 0xf) < CardRank.NUM_RANKS, 'Invalid card rank'
 		assert (value >> 4) < CardSuit.NUM_SUITS, 'Invalid card suit'
 
@@ -27,11 +55,15 @@ class Card:
 		self.value = value
 	
 	def __repr__(self) -> str:
-		"""  """
+		"""Return unicode representation
 		
-		return chr(0x1f0a1 + self.value)
+		See https://en.wikipedia.org/wiki/Playing_cards_in_Unicode#Playing_cards_deck"""
+		
+		if (self.value & 0xf) > CardRank.JACK:
+			return chr(0x1f0a2 + self.value)
+		else: return chr(0x1f0a1 + self.value)
 
-def create_default_deck():
+def create_default_deck() -> List[Card]:
 	"""  """
 
 	return [Card((rank, suit)) for rank in range(CardRank.NUM_RANKS) for suit in range(CardSuit.NUM_SUITS)]
